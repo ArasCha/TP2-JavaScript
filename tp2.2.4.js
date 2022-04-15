c6d9558ba109ba79d6443090a3ed13f44089e94c
Nathan MAICHER <nathan.maicher@etu.univ-nantes.fr>
2022-04-15 23:55:05 +0200

    Commentaires

--- tp2.2.4.js
+++ tp2.2.4.js
@@ -1,25 +1,25 @@
 'use strict';
 
 
-function Album(artist, year, title) {
+function Album(artist, year, title) { //constructeur pour un objet album
     this.artist = artist;
     this.year = year;
     this.title = title;
 }
 
-Album.prototype.getTitle = function getTitle() {
+Album.prototype.getTitle = function getTitle() { //méthode retournant le titre d'un album
     return this.title;
 }
 
-Album.prototype.getArtist = function getArtist() {
+Album.prototype.getArtist = function getArtist() { //méthode retournant l'artiste d'un album
     return this.artist;
 }
 
-Album.prototype.getYear = function getYear() {
+Album.prototype.getYear = function getYear() { //méthode retournant l'année d'un album
     return this.year;
 }
 
-let album = new Album('Fresh Cream', 'Cream', 1966);
+let album = new Album('Fresh Cream', 'Cream', 1966); //création d'un album
     
 console.log(album.getTitle());
 console.log(album.getArtist());
